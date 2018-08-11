d8106b23a2a8b2b0f2eef3369fb79629bc32efbc
Tatsuhiko Seki <osekiharn@gmail.com>
2018-08-11 21:02:07 +0900

    update

--- const/mode.js
+++ const/mode.js
@@ -1,5 +1,6 @@
 export default {
   Major: 'R||9||M3|11||P5||13||M7',
+  Minor: 'R||9|m3||11||P5|b13||m7|',
   'Harmonic Minor': 'R||9|m3||11||P5|b13|||M7',
   'Melodic Minor (Ascending)': 'R||9|m3||11||P5||13||M7',
   'Melodic Minor (Descending)': 'R||9|m3||11||P5|b13||m7|',
